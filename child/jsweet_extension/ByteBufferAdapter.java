29e7993c44f5557e2f4f8584d8542ac02585e577
Louis Grignon <louis.grignon@gmail.com>
2017-12-15 18:48:26 +0100

    adapters

--- child/jsweet_extension/ByteBufferAdapter.java
+++
@@ -1,6 +0,0 @@
-import org.jsweet.transpiler.extension.PrinterAdapter;
-public class ByteBufferAdapter extends PrinterAdapter {
-    public ByteBufferAdapter(PrinterAdapter parent) {
-        super(parent);
-    }
-}
--- child/jsweet_extension/TimeAdapter.java
+++
@@ -1,6 +0,0 @@
-import org.jsweet.transpiler.extension.PrinterAdapter;
-public class TimeAdapter extends PrinterAdapter {
-    public TimeAdapter(PrinterAdapter parent) {
-        super(parent);
-    }
-}
